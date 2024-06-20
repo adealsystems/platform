4641a40f7fe7ade276788159a1bd5d0b788fc5bc
Vlad <vlad@ervion.local>
2024-06-20 09:20:34 +0200

    0.47.6: Fixed JobRecevierRunnable

--- org.adealsystems.platform.orchestrator.engine/src/main/java/org/adealsystems/platform/orchestrator/JobReceiverRunnable.java
+++ org.adealsystems.platform.orchestrator.engine/src/main/java/org/adealsystems/platform/orchestrator/JobReceiverRunnable.java
@@ -74,73 +74,78 @@ public class JobReceiverRunnable implements Runnable {
         while (true) {
             boolean waitingMode = true;
 
-            for (Map.Entry<String, MultipleJobExecutor> entry : asyncJobExecutors.entrySet()) {
-                String queueName = entry.getKey();
-                MultipleJobExecutor jobExecutor = entry.getValue();
-                List<JobMessage> jobs = allJobs.get(queueName);
-                if (jobs == null) {
-                    jobs = new ArrayList<>(); // NOPMD
-                    allJobs.put(queueName, jobs);
-                }
+            try {
+                for (Map.Entry<String, MultipleJobExecutor> entry : asyncJobExecutors.entrySet()) {
+                    String queueName = entry.getKey();
+                    MultipleJobExecutor jobExecutor = entry.getValue();
+                    List<JobMessage> jobs = allJobs.get(queueName);
+                    if (jobs == null) {
+                        jobs = new ArrayList<>(); // NOPMD
+                        allJobs.put(queueName, jobs);
+                    }
 
-                ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest
-                    .builder()
-                    .queueUrl(queueName)
-                    .build();
+                    ReceiveMessageRequest receiveMessageRequest = ReceiveMessageRequest
+                        .builder()
+                        .queueUrl(queueName)
+                        .build();
 
-                List<Message> messages;
-                try {
-                    messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
-                }
-                catch (Exception ex) {
-                    LOGGER.error("Failed to receive messages from queue " + queueName + "!", ex);
-                    continue;
-                }
+                    List<Message> messages;
+                    try {
+                        messages = sqsClient.receiveMessage(receiveMessageRequest).messages();
+                    }
+                    catch (Exception ex) {
+                        LOGGER.error("Failed to receive messages from queue " + queueName + "!", ex);
+                        continue;
+                    }
 
-                if ((messages.isEmpty() && !jobs.isEmpty()) || jobs.size() >= jobBundleSize) {
-                    // No new messages, but collected job requests
-                    // OR more than <jobBundleSize> collected job requests
-                    LOGGER.info("Executing {} collected jobs from {}", jobs.size(), queueName);
-                    executeJobs(jobs, jobExecutor);
-                    jobs.clear();
-                }
+                    if ((messages.isEmpty() && !jobs.isEmpty()) || jobs.size() >= jobBundleSize) {
+                        // No new messages, but collected job requests
+                        // OR more than <jobBundleSize> collected job requests
+                        LOGGER.info("Executing {} collected jobs from {}", jobs.size(), queueName);
+                        executeJobs(jobs, jobExecutor);
+                        jobs.clear();
+                    }
 
-                // Read all available requests
-                if (!messages.isEmpty()) {
-                    waitingMode = false;
-                    for (Message message : messages) {
-                        LOGGER.debug("Processing message {}", message);
+                    // Read all available requests
+                    if (!messages.isEmpty()) {
+                        waitingMode = false;
+                        for (Message message : messages) {
+                            LOGGER.debug("Processing message {}", message);
 
-                        String messageBody = message.body();
-                        JobMessage job;
-                        try {
-                            job = OBJECT_MAPPER.readValue(messageBody, JobMessage.class);
+                            String messageBody = message.body();
+                            JobMessage job;
+                            try {
+                                job = OBJECT_MAPPER.readValue(messageBody, JobMessage.class);
+                            }
+                            catch (JsonProcessingException ex) {
+                                LOGGER.error("Error reading message body {}!", messageBody, ex);
+                                continue;
+                            }
+                            finally {
+                                deleteMessage(message, queueName);
+                            }
+
+                            jobs.add(job);
                         }
-                        catch (JsonProcessingException ex) {
-                            LOGGER.error("Error reading message body {}!", messageBody, ex);
-                            continue;
-                        }
-                        finally {
-                            deleteMessage(message, queueName);
-                        }
-
-                        jobs.add(job);
                     }
                 }
-            }
 
-            // delay before next check
-            try {
-                if (waitingMode) {
-                    sleep(waitingInterval);
+                // delay before next check
+                try {
+                    if (waitingMode) {
+                        sleep(waitingInterval);
+                    }
+                    else {
+                        // sleep short
+                        sleep(100);
+                    }
                 }
-                else {
-                    // sleep short
-                    sleep(100);
+                catch (InterruptedException ex) {
+                    break;
                 }
             }
-            catch (InterruptedException ex) {
-                break;
+            catch(Throwable th) {
+                LOGGER.error("Error occurred inside thread loop!", th);
             }
         }
     }
@@ -153,7 +158,12 @@ public class JobReceiverRunnable implements Runnable {
             dataIds[i] = DataIdentifier.fromString(dataId);
         }
 
-        jobExecutor.execute(dataIds);
+        try {
+            jobExecutor.execute(dataIds);
+        }
+        catch(Throwable th) {
+            LOGGER.error("Error executing job(s) {}", jobs, th);
+        }
     }
 
     private void sleep(long value) throws InterruptedException {
